6d916e4ba551318cc48c9a559696a01626fd9c58
JadyLinnit <linnitjady@gmail.com>
2021-11-16 10:54:13 -0300

    CREATED Model-Repositories-Controllers-Security-Service

---
+++ src/main/java/com/postagem/atvpostagem/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.postagem.atvpostagem.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.postagem.atvpostagem.model.Usuario;
+import com.postagem.atvpostagem.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) {
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+		
+		return user.map(UserDetailsImpl::new).get();
+	}
+}
