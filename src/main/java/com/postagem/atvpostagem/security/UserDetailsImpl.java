2e3fd45038de0d087fc17ed707a8e8872245e2f4
JadyLinnit <linnitjady@gmail.com>
2021-12-10 23:22:46 -0300

    Update

--- src/main/java/com/postagem/atvpostagem/security/UserDetailsImpl.java
+++ src/main/java/com/postagem/atvpostagem/security/UserDetailsImpl.java
@@ -1,9 +1,11 @@
 package com.postagem.atvpostagem.security;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import com.postagem.atvpostagem.model.Usuario;
 
 public class UserDetailsImpl implements UserDetails{
@@ -12,6 +14,8 @@ public class UserDetailsImpl implements UserDetails{
 	
 	private String userName;
 	private String password;
+	private List<GrantedAuthority> authorities;
+	
 	
 	
 	
@@ -29,7 +33,7 @@ public class UserDetailsImpl implements UserDetails{
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	@Override
